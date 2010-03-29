e3ff95c2b8442beddbb774665c5874173fe666c9
elij <elij.mx@gmail.com>
2010-03-28 19:39:18 -0700

    added build target for python-ctypes module

---
+++ examples/python/ctypes/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyCronkite',
+    version = '1.0',
+    description = 'python module for libcronkite',
+    url = 'http://github.com/cactus/cronkite',
+    py_modules = ['pycronkite'])
+
