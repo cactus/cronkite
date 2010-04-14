c85474ceba6745651e897f873c45e1a8d3e20620
elij <elij.mx@gmail.com>
2010-04-13 23:50:59 -0700

    normalized python module names, and added more testing

---
+++ examples/python-tests/tests.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import unittest
+import exceptions
+
+### setup path for load library
+sys.bytecodebase = None
+sys.path.append('result')
+
+import cronkite
+
+class TestCases(unittest.TestCase):
+    def setUp(self):
+        cronkite.seturl('http://aur.test')
+
+    def test_example(self):
+        """Perform a test query, and validate results"""
+        x = cronkite.query('s', 'example')
+        self.assertTrue(len(x) == 1)
+        self.assertTrue(x[0]['name'] == 'example')
+
+    def test_errors(self):
+        """Ensure cronkite.query throws typeerror"""
+        self.assertRaises(
+            exceptions.TypeError, cronkite.query, ('wrong', 'example'))
+
