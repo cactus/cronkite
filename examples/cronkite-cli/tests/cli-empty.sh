4a9627c8db423614679a72287e7bd4bdf3f8ec2a
elij <elij.mx@gmail.com>
2010-04-15 20:31:46 -0700

    added a couple more tests.
    added the following tests:
      cli-empty - test behavior on an empty result. currently segfaults.
                  hopefully this would result in a 500 level error from the
                  webserver. Will work on code to handle this case later.
      cli-notjson - test behavior on an file that is not json formatted data.

---
+++ examples/cronkite-cli/tests/cli-empty.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OUTPUT=$(${1} -search example 2>&1)
+rval=$?
+# echo it out once just for ctest -V output
+echo $OUTPUT
+if [ $rval -eq 0 ]; then
+    exit 0;
+else
+    exit 1;
+fi
---
+++ examples/cronkite-cli/tests/cli-notjson.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+OUTPUT=$(${1} -search example 2>&1)
+rval=$?
+# echo it out once just for ctest -V output
+echo $OUTPUT
+echo $OUTPUT | egrep -qi 'error parsing json result'
+nval=$?
+if [ $rval -eq 1 ] && [ $nval -eq 0 ]; then
+    exit 0;
+else
+    exit 1;
+fi
