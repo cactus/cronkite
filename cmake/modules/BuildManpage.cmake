1b783cb368d607621348d5540487e3db2d836979
elij <elij.mx@gmail.com>
2010-04-16 21:50:39 -0700

    converting from a2x (asciidoc) to pod2man (perl-pod).
    Much easier to generate man pages, and more people have it installed
    already.

--- cmake/modules/BuildManpage.cmake
+++ cmake/modules/BuildManpage.cmake
@@ -1,16 +1,20 @@
-macro(a2x_build_manpage _depends_N _mps_N _mpt_N mversion)
-    get_filename_component(_mpt_num ${_mpt_N} EXT)
-    string(REPLACE "." "" _mpt_num "${_mpt_num}")
-    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/${_mps_N} 
-         DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
-         USE_SOURCE_PERMISSIONS)
-    add_custom_command(
-        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_mpt_N}
-        COMMAND ${A2X} --asciidoc-opts="-a revnumber=${mversion}" -d manpage -f manpage -D ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR}/${_mps_N}
-        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${_mps_N})
-    install(
-        FILES ${CMAKE_CURRENT_BINARY_DIR}/${_mpt_N}
-        DESTINATION "share/man/man${_mpt_num}"
-        PERMISSIONS OWNER_READ GROUP_READ WORLD_READ)
-    set(${_depends_N} ${${_depends_N}} ${CMAKE_CURRENT_BINARY_DIR}/${_mpt_N})
-endmacro(a2x_build_manpage _depends_N mptarg_N mpsrc_N mversion)
+macro(p2m_build _depends_N _p_input mversion)
+    if(POD2MAN_FOUND)
+        get_filename_component(_p_tmp ${_p_input} EXT)
+        get_filename_component(_oname ${_p_input} NAME_WE)
+        # should be something like .1.pod now
+        string(SUBSTRING ${_p_tmp} 1 1 _m_num)
+        set(ofile ${CMAKE_CURRENT_BINARY_DIR}/${_oname}.${_m_num})
+
+        add_custom_command(
+            OUTPUT ${ofile}
+            COMMAND ${POD2MAN_EXECUTABLE}
+                -s ${_m_num} -n ${_oname}
+                -c ${_oname} -r "${mversion}" ${_p_input} ${ofile})
+        install(
+            FILES ${ofile}
+            DESTINATION "share/man/man${_m_num}"
+            PERMISSIONS OWNER_READ GROUP_READ WORLD_READ)
+        set(${_depends_N} ${${_depends_N}} ${ofile})
+    endif(POD2MAN_FOUND)
+endmacro(p2m_build _depends_N _p_input mversion)
